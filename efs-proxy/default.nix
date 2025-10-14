786e9bf85c144c1d6d2676994fe1ccaefed54726
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:22:25 +1000

    fix: copilot suggests

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -7,13 +7,12 @@ let
     rev = "v2.3.3";
     sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
   };
-  src_folder = efs-utils_src;
-  src_file = efs-utils_src + "/src/proxy/Cargo.lock";
+  src_folder = lib.sourceByRegex efs-utils_src [ "^src/proxy/.*" ];
 in
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "efs-proxy";
   version = "2.3.3";
   src = src_folder;
-  cargoLock.lockFile = src_file;
+  cargoLock.lockFile = "src/proxy/Cargo.lock";
 }

dc33c989b7ed9f072533fb2a20e08cf2941a6b05
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:15:05 +1000

    fix: Src file

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -7,12 +7,12 @@ let
     rev = "v2.3.3";
     sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
   };
-  src_folder = efs-utils_src + "/src/proxy" // pkgs.lib.cleanSource ./.;
+  src_file = efs-utils_src + "/src/proxy/Cargo.lock" // pkgs.lib.cleanSource ./.;
 in
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "efs-proxy";
   version = "2.3.3";
   src = src_folder;
-  cargoLock.lockFile = ${src_folder}/Cargo.lock;
+  cargoLock.lockFile = src_file;
 }

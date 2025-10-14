aad34468738712952eeedf2ace82f4257884ea48
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:16:41 +1000

    fix: separate variables

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -7,7 +7,8 @@ let
     rev = "v2.3.3";
     sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
   };
-  src_file = efs-utils_src + "/src/proxy/Cargo.lock" // pkgs.lib.cleanSource ./.;
+  src_folder = efs-utils_src + "/src/proxy" // pkgs.lib.cleanSource ./.;
+  src_file = efs-utils_src + "/src/proxy/Cargo.lock";
 in
 
 pkgs.rustPlatform.buildRustPackage rec {

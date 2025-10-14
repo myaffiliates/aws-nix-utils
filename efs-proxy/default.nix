203ec39170653701c370a7764531503e0dc1736f
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:58:09 +1000

    fas

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -14,5 +14,5 @@ pkgs.rustPlatform.buildRustPackage rec {
   version = "2.3.3";
   src = efs-utils_src + "/src/proxy";
   cargoLock.lockFile = src + "/Cargo.lock";
-  buildInputs = [ pkg-config pkgs.openssl ];
+  buildInputs = [ pkgs.pkg-config pkgs.openssl ];
 }

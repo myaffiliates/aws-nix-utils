a5031ee2f66d7797939573bfa4160ced6c141bf7
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:32:08 +1000

    gdhfs

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -7,12 +7,11 @@ let
     rev = "v2.3.3";
     sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
   };
-  src_folder = lib.sourceByRegex efs-utils_src [ "^src/proxy/.*" ];
 in
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "efs-proxy";
   version = "2.3.3";
-  src = src_folder;
-  cargoLock.lockFile = src_folder + "/Cargo.lock";
+  src = efs-utils_src;
+  cargoLock.lockFile = efs-utils_src + "/src/proxy/Cargo.lock";
 }

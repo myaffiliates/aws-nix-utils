bc1371db542af0070e569c21d03d05d4ed2c8aaa
Edward Palmer <edward@myaffiliates.com>
2025-10-14 23:12:12 +1000

    fix: Sourcing folder

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -1,13 +1,18 @@
 { lib, stdenv, pkgs, fetchFromGitHub }:
 
+let
+  efs-utils_src = fetchFromGitHub {
+    owner = "aws";
+    repo = "efs-utils";
+    rev = "v2.3.3";
+    sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
+  };
+  src_folder = efs-utils_src + "/src/proxy" // pkgs.lib.cleanSource ./.;
+in
+
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "efs-proxy";
   version = "2.3.3";
-  src = fetchFromGitHub {
-    owner = "aws";
-    repo = "efs-utils";
-    rev = "v${version}";
-    sha256 = "sha256-OIf5GZt8pVPQDQ89mFa1e165e65N1X307D9HQ23fASQ=";
-  };
-  cargoLock.lockFile = ${src}/src/proxy/Cargo.lock;
+  src = src_folder;
+  cargoLock.lockFile = ${src_folder}/Cargo.lock;
 }

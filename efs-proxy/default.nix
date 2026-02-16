105187f45a2e963c7f83139ab65ac8e8727c9ebe
Edward Palmer <edward@myaffiliates.com>
2026-02-16 13:04:15 +1000

    fix: EFS proxy

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, pkgs, pkg-config, fetchFromGitHub, runCommand }:
+{ lib, stdenv, pkgs, pkg-config, fetchFromGitHub }:
 
 let
   efs-utils_src = fetchFromGitHub {
@@ -7,77 +7,45 @@ let
     rev = "v2.4.1";
     sha256 = "sha256-3GfrBY9h0ALwn9E2LwfxKgT8QdMoiBRGgzFZQN3ujKQ=";
   };
-  
-  # Patch source to downgrade aws-lc-rs to avoid aws-lc-fips-sys
-  patched-src = runCommand "efs-proxy-src-patched" {} ''
-    cp -r ${efs-utils_src}/src/proxy $out
-    chmod -R +w $out
-    
-    # Downgrade aws-lc-rs to 1.9.0 which doesn't have aws-lc-fips-sys dependency
-    sed -i 's/aws-lc-rs = { version = "1.11.0", features = \["fips"\] }/aws-lc-rs = "1.9.0"/g' $out/Cargo.toml
-    
-    # Update Cargo.lock to change aws-lc-rs from 1.11.0 to 1.9.0
-    sed -i 's/name = "aws-lc-rs"[[:space:]]*\nversion = "1.11.0"/name = "aws-lc-rs"\nversion = "1.9.0"/g' $out/Cargo.lock
-    
-    # Remove aws-lc-fips-sys package entry from Cargo.lock
-    # This is a multi-line block, so we use awk
-    awk '
-      BEGIN { skip = 0 }
-      /^\[\[package\]\]$/ { 
-        # Start capturing the package block
-        block = $0 "\n"
-        in_package = 1
-        next
-      }
-      in_package {
-        block = block $0 "\n"
-        if (/^name = "aws-lc-fips-sys"/) {
-          skip = 1
-        }
-        if (/^$/ || /^\[\[/) {
-          # End of package block
-          if (!skip) {
-            printf "%s", block
-          }
-          skip = 0
-          in_package = 0
-          block = ""
-          if (/^\[\[/) {
-            # This is the start of next package
-            block = $0 "\n"
-            in_package = 1
-          }
-        }
-        next
-      }
-      { print }
-    ' $out/Cargo.lock > $out/Cargo.lock.tmp && mv $out/Cargo.lock.tmp $out/Cargo.lock
-  '';
 in
 
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "efs-proxy";
-  version = "2.4.1";
-  src = patched-src;
-  
-  cargoHash = "sha256-2oP0rlSdzIcffl5PNuGKwr5vQLhn6IPvTTOTfw8yWdA=";
+# aws-lc-fips-sys 0.13.9 has known build failures with modern toolchains
+# See: https://github.com/aws/aws-lc-rs/issues/569
+# Only build on platforms where it works
+if stdenv.hostPlatform.isAarch64 then
+  pkgs.rustPlatform.buildRustPackage rec {
+    pname = "efs-proxy";
+    version = "2.4.1";
+    src = efs-utils_src + "/src/proxy";
+    
+    cargoLock = { lockFile = src + "/Cargo.lock"; };
 
-  nativeBuildInputs = [
-    pkgs.pkg-config
-    pkgs.go
-    pkgs.cmake
-    pkgs.perl
-  ];
+    nativeBuildInputs = [
+      pkgs.pkg-config
+      pkgs.go
+      pkgs.cmake
+      pkgs.perl
+    ];
 
-  buildInputs = [
-    pkgs.openssl
-  ];
+    buildInputs = [
+      pkgs.openssl
+    ];
 
-  OPENSSL_DIR = pkgs.openssl.dev;
-  OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    OPENSSL_DIR = pkgs.openssl.dev;
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
 
-  # Disable hardening
-  hardeningDisable = [ "fortify" ];
+    hardeningDisable = [ "fortify" ];
 
-  doCheck = false;
-}
+    doCheck = false;
+  }
+else
+  # Stub package for unsupported platforms
+  pkgs.runCommand "efs-proxy-unsupported" {} ''
+    mkdir -p $out/bin
+    cat > $out/bin/efs-proxy << 'EOF'
+#!/bin/sh
+echo "efs-proxy not available on this platform due to aws-lc-fips-sys build issues" >&2
+exit 1
+EOF
+    chmod +x $out/bin/efs-proxy
+  ''

b2b398f298be6936c80b70f608537bbb710b8b40
Edward Palmer <edward@myaffiliates.com>
2026-02-16 12:31:37 +1000

    fix: EFS proxy

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, pkgs, pkg-config, fetchFromGitHub }:
+{ lib, stdenv, pkgs, pkg-config, fetchFromGitHub, runCommand }:
 
 let
   efs-utils_src = fetchFromGitHub {
@@ -7,14 +7,23 @@ let
     rev = "v2.4.1";
     sha256 = "sha256-3GfrBY9h0ALwn9E2LwfxKgT8QdMoiBRGgzFZQN3ujKQ=";
   };
+  
+  # Patch source to remove FIPS feature from aws-lc-rs
+  patched-src = runCommand "efs-proxy-src-patched" {} ''
+    cp -r ${efs-utils_src}/src/proxy $out
+    chmod -R +w $out
+    
+    # Remove fips feature from Cargo.toml
+    sed -i 's/aws-lc-rs = { version = "1.11.0", features = \["fips"\] }/aws-lc-rs = "1.11.0"/g' $out/Cargo.toml
+  '';
 in
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "efs-proxy";
   version = "2.4.1";
-  src = efs-utils_src + "/src/proxy";
+  src = patched-src;
   
-  cargoLock = { lockFile = src + "/Cargo.lock"; };
+  cargoHash = "sha256-gOJmZ0CDvdgnJxLtXyTwWm0zLEPUGgEO/8D8YPHegSw=";
 
   nativeBuildInputs = [
     pkgs.pkg-config
@@ -32,27 +41,6 @@ pkgs.rustPlatform.buildRustPackage rec {
 
   # Disable hardening
   hardeningDisable = [ "fortify" ];
-
-  # Remove FIPS feature and aws-lc-fips-sys dependency
-  # aws-lc-fips-sys 0.13.9 has compilation issues with modern toolchains
-  postPatch = ''
-    # Remove fips feature flag from aws-lc-rs dependency
-    sed -i 's/aws-lc-rs = { version = "1.11.0", features = \["fips"\] }/aws-lc-rs = { version = "1.11.0" }/g' Cargo.toml
-    
-    # Remove aws-lc-fips-sys entries from Cargo.lock entirely
-    # Use awk to remove package sections that have name = "aws-lc-fips-sys"
-    awk '
-      /^\[\[package\]\]$/ { in_package = 1; package_start = NR }
-      in_package && /^name = "aws-lc-fips-sys"/ { skip_package = 1 }
-      in_package && NR > package_start && /^\[\[package\]\]$/ { 
-        in_package = 1; 
-        package_start = NR; 
-        skip_package = 0 
-      }
-      !skip_package { print }
-      /^$/ && skip_package { skip_package = 0 }
-    ' Cargo.lock > Cargo.lock.tmp && mv Cargo.lock.tmp Cargo.lock
-  '';
 
   doCheck = false;
 }

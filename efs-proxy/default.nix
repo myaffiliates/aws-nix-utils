7a8a1c147e9d9049dd5bd4299dfb763392ed3f0d
Edward Palmer <edward@myaffiliates.com>
2026-02-16 12:26:51 +1000

    fix: EFS proxy

--- efs-proxy/default.nix
+++ efs-proxy/default.nix
@@ -30,8 +30,29 @@ pkgs.rustPlatform.buildRustPackage rec {
   OPENSSL_DIR = pkgs.openssl.dev;
   OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
 
-  # Disable hardening - aws-lc-fips-sys can fail with fortify on some systems
+  # Disable hardening
   hardeningDisable = [ "fortify" ];
+
+  # Remove FIPS feature and aws-lc-fips-sys dependency
+  # aws-lc-fips-sys 0.13.9 has compilation issues with modern toolchains
+  postPatch = ''
+    # Remove fips feature flag from aws-lc-rs dependency
+    sed -i 's/aws-lc-rs = { version = "1.11.0", features = \["fips"\] }/aws-lc-rs = { version = "1.11.0" }/g' Cargo.toml
+    
+    # Remove aws-lc-fips-sys entries from Cargo.lock entirely
+    # Use awk to remove package sections that have name = "aws-lc-fips-sys"
+    awk '
+      /^\[\[package\]\]$/ { in_package = 1; package_start = NR }
+      in_package && /^name = "aws-lc-fips-sys"/ { skip_package = 1 }
+      in_package && NR > package_start && /^\[\[package\]\]$/ { 
+        in_package = 1; 
+        package_start = NR; 
+        skip_package = 0 
+      }
+      !skip_package { print }
+      /^$/ && skip_package { skip_package = 0 }
+    ' Cargo.lock > Cargo.lock.tmp && mv Cargo.lock.tmp Cargo.lock
+  '';
 
   doCheck = false;
 }
